00739cdb6db401c94dfa0aa34c6361e7db67ba7e
kimseonggyu1 <luckykimsk@gmail.com>
2020-10-01 13:43:44 +0900

    feat: vuetify 적용 및 NavigationBar 구현

--- src/front/vue.config.js
+++ src/front/vue.config.js
@@ -1,15 +1,16 @@
-const path = require("path");
-
 module.exports = {
-    outputDir: path.resolve(__dirname, "../" + "main/resources/static"),
-    devServer: {
-        host: "localhost",
-        proxy: {
-            "/api": {
-                target: "http://localhost:8080",
-                ws: true,
-                changeOrigin: true
-            }
-        }
+  "outputDir": "/home/ksg/Desktop/gitHub/gallery/src/main/resources/static",
+  "devServer": {
+    "host": "localhost",
+    "proxy": {
+      "/api": {
+        "target": "http://localhost:8080",
+        "ws": true,
+        "changeOrigin": true
+      }
     }
-};
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
