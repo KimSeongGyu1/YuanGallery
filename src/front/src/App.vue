b27f802dd33a5bee2e69f20dade8b7f1a71a6de2
kimseonggyu1 <luckykimsk@gmail.com>
2020-10-09 13:58:45 +0900

    feat: 푸터 추가

--- src/front/src/App.vue
+++ src/front/src/App.vue
@@ -2,17 +2,20 @@
   <v-app>
     <NavigationBar/>
     <router-view/>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
 import NavigationBar from './components/NavigationBar';
+import Footer from "./components/Footer";
 
 export default {
   name: 'App',
 
   components: {
     NavigationBar,
+    Footer
   },
 
   data: () => ({
