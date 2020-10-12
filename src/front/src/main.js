688fde76b02a754ad2d5f0570d323859ee47ccb8
KimSeongGyu1 <luckykimsk@gmail.com>
2020-10-12 23:51:11 +0900

    chore: 구글 어날리틱스 적용 (#11)

--- src/front/src/main.js
+++ src/front/src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import {router} from './router'
 import VueCookie from 'vue-cookie'
+import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = false
 
 Vue.use(VueCookie);
+
+Vue.use(VueAnalytics, {
+  id: "UA-136033416-2",
+  router
+})
 
 new Vue({
   vuetify,
